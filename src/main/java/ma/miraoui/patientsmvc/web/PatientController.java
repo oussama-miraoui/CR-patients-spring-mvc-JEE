1709f919f0177cd6ad23928990a741210a8c93ee
oussama_miraoui <oussamamiraoui@gmail.com>
2022-04-10 04:30:06 +0100

    adding => add patient, edit patient and form validation

--- src/main/java/ma/miraoui/patientsmvc/web/PatientController.java
+++ src/main/java/ma/miraoui/patientsmvc/web/PatientController.java
@@ -7,9 +7,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -43,5 +46,29 @@ public class PatientController {
         patientRepository.deleteById(id);
         return "redirect:/index?page"+page+"&keyword="+keyword;
 
+    }
+
+    @GetMapping(path = "/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient", new Patient());
+        return "formPatients";
+    }
+
+    @PostMapping(path = "/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult, int page, String keyword){
+        if(bindingResult.hasErrors()){
+            return "formPatients";
+        }
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping(path = "/editPatient")
+    public String editPatient(Long id, Model model , int page, String keyword){
+        Patient patient = patientRepository.findById(id).orElse(null);
+        model.addAttribute("patient", patient);
+        model.addAttribute("page", page);
+        model.addAttribute("keyword", keyword);
+        return "editPatient";
     }
 }
